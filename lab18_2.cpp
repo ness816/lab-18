6be22081b85223089472fdb84aa87cdd9a0d08f2
ness816 <ness41861@gmail.com>
2024-02-09 17:24:29 +0700

    ไม่มีTAช่วยไม่ได้กลับ

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,7 +5,11 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap(Rect *R1,Rect *R2)
+{
+	double  x_distance = max(R1->x, R2->x) - min(R1->x + R1->w , R2->x + R2->w);
+	double y_distance = min(R1->y, R2->y) - max(R1->y - R1->h , R2->y - R2->h);
+	return  x_distance > 0 && y_distance > 0? 0:abs(x_distance)*abs(y_distance);
 
 
 
